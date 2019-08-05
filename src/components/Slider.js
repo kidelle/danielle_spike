c909e730126a6d59ca9d890698ae10da105be889
Danielle Martain <dmartain@gmail.com>
2019-08-05 08:00:30 -0500

    set up component and server

---
+++ src/components/Slider.js
@@ -0,0 +1,16 @@
+import React, {Component } from 'react';
+
+
+class Slider extends Component {
+
+    render() {
+        return (
+            <div className="App">
+            <h1>Slider</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Slider;
